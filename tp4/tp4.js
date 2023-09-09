071db4f9d8625c91177e862b3995693f6f438d39
ivansalda <83249525+ivansalda@users.noreply.github.com>
2023-09-09 15:38:41 -0300

    Add files via upload

---
+++ tp4/tp4.js
@@ -0,0 +1,17 @@
+/* Necesito:
+ -fondo
+ -Dinosourio
+ -Terodactilo
+ -Captus
+ -Rompe objetivos (llamarada, pisoton, lo que sea)
+ */
+ let A1[]= loadImage() [6];
+function setup() {
+  createCanvas (1200,600);
+}
+
+
+function draw() {
+  background (0);
+  
+}
