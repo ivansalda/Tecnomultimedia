12ed8be0cc556dafd35f1f12c1dced72de2df1e4
ivansalda <83249525+ivansalda@users.noreply.github.com>
2023-11-28 13:36:32 -0300

    Add files via upload

--- Boton.js
+++ Boton.js
@@ -1,25 +1,38 @@
 class Boton {
-  constructor () {
+  constructor (xp,yp,xa,ya,tx) {
     //this.opasidad = 50;
     // this.colision;
+    this.xp = xp;
+    this.yp = yp;
+    this.xa = xa;
+    this.ya = ya;
+    this.tx = tx;
+    this.op = 80;
   }
 
-  dibujar (xp, yp, tx) {
+  dibujar () {
     push ();
     textSize (37);
     fill(0);
     push ();
     strokeWeight (3);
-    stroke(255, 207, 13);
-    rect (xp, yp, 175, 75);
+    stroke(255, 207, 13,this.op);
+    rect (this.xp, this.yp, this.xa, this.ya);
     pop ();
-
-    fill (255, 207, 13);
-    text (tx, xp+10, yp+55);
+    fill (255, 207, 13,this.op);
+    text (this.tx, this.xp+10, this.yp+55);
     pop();
+    if (this.colisiones ()){
+    this.op =255;
+    } else {
+    this.op = 80;
+    }
   }
   colisiones () {
-    if (mouseX <= 575 && mouseX >= 400 && mouseY <=  575 && mouseY >= 500) {
+    if (mouseX <= this.xp + this.xa && mouseX >= this.xp && mouseY <=  this.yp + this.ya && mouseY >= this.yp) {      
+      return true;   
+    }else {
+    return false; 
     }
   }
 }
